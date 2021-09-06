cd084404c848c4044d7756c95b5e9bcf106b1af6
Ensar Dogrusoz <42221230+ensardogrusoz@users.noreply.github.com>
2021-09-06 16:32:22 -0400

    Add files via upload

---
+++ rpsGame.py
@@ -0,0 +1,33 @@
+# we import random bc we want the computer to randomly select Rock, Paper or Scissors
+import random
+
+while True:
+    #ask the user a question then wait for their input
+    user_action = input("Enter a choice (rock, paper, scissors): ")
+    possible_actions = ["rock", "paper", "scissors"]
+    computer_action = random.choice(possible_actions)
+    print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+    #this is the winning logic
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if computer_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if computer_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if computer_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    # asks the user if they want to play again
+    play_again = input("Play again? (y/n): ")
+    if play_again.lower() != "y":
+        break
